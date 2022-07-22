615da8fb8b230db35c45f42f61ad2265a530256e
Thiago Brandão <194487+thiagobrandam@users.noreply.github.com>
2022-07-22 18:34:58 -0300

    Basic structure

---
+++ lib/stoffle.rb
@@ -0,0 +1,4 @@
+module Stoffle; end
+
+require_relative './stoffle/token'
+require_relative './stoffle/lexer'
---
+++ test/stoffle/lexer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LexerTest < Test::Unit::TestCase
+  def test_it_does_something_useful
+    assert true
+  end
+end
